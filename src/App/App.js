08de53e507224c0db27d453597005bee30b59b60
fedorrychkov <fedorrychkov@yandex.ru>
2018-08-10 18:06:58 +0300

    add react router

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,18 @@
 // @flow
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+
+import AuthPage from './Pages/Auth/AuthPage';
+
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <section className="App">
+      <Switch>
+        <Route path='/auth' component={AuthPage}/>
+      </Switch>
+    </section>
   );
 }
 
---
+++ src/App/Pages/Auth/AuthPage.js
@@ -0,0 +1,12 @@
+//@flow
+
+import React from 'react';
+import './index.css';
+
+const AuthPage = () => {
+    return (
+        <div>is AuthPage</div>
+    );
+};
+
+export default AuthPage;
