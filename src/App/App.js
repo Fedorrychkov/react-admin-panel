9c54fe95fbb5909fc31fcfd211eef4267a8c22d8
fedorrychkov <fedorrychkov@yandex.ru>
2018-08-13 00:59:58 +0300

    update flowtype

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,31 @@
 // @flow
-import React from 'react';
+import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import AuthPage from './Pages/Auth/AuthPage';
 import TopBar from './components/TopBar/TopBar';
 
-const App = () => {
-  return (
-    <section className="App layout__light">
-      <TopBar/>
-      <Switch>
-        <Route path='/auth' component={AuthPage}/>
-      </Switch>
-    </section>
-  );
+type State = {
+  isLight: boolean,
+};
+
+class App extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = { isLight: true };
+  }
+
+
+  render() {
+    return (
+      <section className={`App ${this.state.isLight? 'layout__light' : 'layout__dark'}`}>
+        <TopBar/>
+        <Switch>
+          <Route path='/auth' component={AuthPage}/>
+        </Switch>
+      </section>
+    );
+  }
 }
 
 export default App;
