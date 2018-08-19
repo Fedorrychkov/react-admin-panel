1edcffd1391ba0cf2f7af849c08d4fc909fd7421
fedorrychkov <fedorrychkov@yandex.ru>
2018-08-19 16:09:30 +0300

    update action, add smart topbar container, remove old layouttheme

--- src/App/store/layout/actions.js
+++ src/App/store/layout/actions.js
@@ -5,10 +5,13 @@ import { LIGHT_LAYOUT, DARK_LAYOUT } from './actionTypes';
 export function changeTheme(state: string) {
     switch (state) {
         case LIGHT_LAYOUT:
+            localStorage.setItem('f-panel.theme', DARK_LAYOUT);
             return { type: DARK_LAYOUT };
         case DARK_LAYOUT:
+            localStorage.setItem('f-panel.theme', LIGHT_LAYOUT);
             return { type: LIGHT_LAYOUT };
         default:
+            localStorage.setItem('f-panel.theme', LIGHT_LAYOUT);
             return { type: LIGHT_LAYOUT };
     }
 }
