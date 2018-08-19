0daaa4795049930f63999507aff6ef10aa95d17a
fedorrychkov <fedorrychkov@yandex.ru>
2018-08-19 16:08:51 +0300

    add stupid and smart component

--- src/App/App.js
+++ src/App/App.js
@@ -1,56 +1,14 @@
 // @flow
-import * as React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, {Fragment} from 'react';
 
-import AuthPage from './Pages/Auth/AuthPage';
-import TopBar from './components/TopBar/TopBar';
-import { store } from './store/reducers';
-import { DARK_LAYOUT, LIGHT_LAYOUT } from './store/layout/actionTypes';
+import SmartLayoutTheme from './containers/SmartLayoutTheme/SmartLayoutTheme';
 
-type State = {
-  dark: boolean,
-  store: any,
-};
-
-class App extends React.Component<{}, State> {
-  constructor(props: any) {
-    super(props);
-    this.storeChange = this.storeChange.bind(this);
-    this.state = { dark: false, store: store.subscribe(this.storeChange) };
-  }
-
-  componentWillMount() {
-    this.storeChange();
-  }
-
-  storeChange() {
-    let currentValue = store.getState();
-    let theme = currentValue.layoutTheme.currentLayout;
-    switch (theme) {
-      case DARK_LAYOUT:
-        this.setState({dark: true});
-        break;
-      case LIGHT_LAYOUT:
-        this.setState({dark: false});
-        break;
-      default:
-        this.setState({dark: false});
-        break;
-    }
-  }
-
-  render() {
+const App = () => {
     return (
-      <section className={`App layout ${this.state.dark? 'layout__dark' : 'layout__light'}`}>
-        <TopBar/>
-        <section className="content">
-          <Switch>
-            <Route path='/auth' component={AuthPage}/>
-          </Switch>
-        </section>
-      </section>
+      <Fragment>
+        <SmartLayoutTheme/>
+      </Fragment>
     );
   }
-}
 
 export default App;
